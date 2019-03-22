8ef0f6e3418e9129fd6058dd17ebac61863bc89e
Andre Mazal Krauss <amk1710@gmail.com>
2019-03-22 01:26:55 -0300

    testes de tecnologia: touch e save state(com player preffs)

---
+++ Assets/Scripts/Teste/Save_Load.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//script bobão para testar funcionalidade de Load-Save usando PlayerPrefs, que acho que será suficiente para nosso projeto. Ass: Krauss
+public class Save_Load : MonoBehaviour
+{
+    public InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //geralmente poderia botar tanto Save quanto Load como funções static, que recebem uma referência para a classe que contém as informações a serem salvas.
+    // Mas esse exemplo é tão bobo que ia ficar só redundante, inútil e confuso dar essa volta
+    
+    public /*static */ void SaveState(/*informação a ser salva */)
+    {
+        if(inputField != null)
+        {
+            PlayerPrefs.SetString("characterName", inputField.text);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public /*static */ void LoadState(/* referencia para lugar onde guardar info */)
+    {
+        if(inputField != null)
+        {
+            inputField.text = PlayerPrefs.GetString("characterName", "No player saved");
+        }
+    }
+}
---
+++ Assets/Scripts/Teste/TouchInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    
+    public GameObject firework;
+    public GameObject firework2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch began");
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                pos.z = 0.0f;
+                GameObject go = GameObject.Instantiate(firework2, pos, Quaternion.identity);
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("touch moved");
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch ended");
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                pos.z = 0.0f;
+                GameObject go = GameObject.Instantiate(firework, pos, Quaternion.identity);
+            }
+
+        }
+    }
+}
