64b63299eb5bfbf15c378ef318d958cd6b01f3cd
Hugo Tonette <hugo.tonette@gmail.com>
2019-03-29 03:17:54 -0300

    Navegabilidade * Add scenes e preparei para seguir o diagrama de navegabilidade feito pelos GDs

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+	[HideInInspector]
+	public string SelectedLevel;
+
+
+	public void ToNextScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+
+	public void ToPauseMenu()
+	{
+		SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+	}
+
+	public void ExitPauseMenu()
+	{
+		SceneManager.LoadScene("Countdown", LoadSceneMode.Additive);
+		SceneManager.UnloadSceneAsync("PauseMenu");
+	}
+
+	public void ToGame(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		SelectedLevel = sceneName;
+		SceneManager.LoadScene("Countdown", LoadSceneMode.Additive);
+	}
+
+	public void ToPlayAgain()
+	{
+		SceneManager.LoadScene(SelectedLevel, LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+	[Range(0, 100)] public float CountdownTime = 2;
+
+	private IEnumerator CountdownTimer()
+	{
+		yield return new WaitForSeconds(CountdownTime);
+		SceneManager.UnloadSceneAsync("Countdown");
+	}
+
+	private void Start()
+	{
+		StartCoroutine("CountdownTimer");
+	}
+}
