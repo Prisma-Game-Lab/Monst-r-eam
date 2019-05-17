500471e3c8852e8ae60019c22bd7ab0d694ed5e0
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-05-16 23:26:23 -0300

    sistema de progressao

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,15 +9,27 @@ public class LevelManager : MonoBehaviour
     public GameObject ButtonNext;
     public GameObject ButtonPrevious;
     private int currentPage;
+    [HideInInspector]
+    public List<int> TotalStars;
+    public List<int> StarsToNextPage;
     //public GameObject[] ButtonsLevels;
 
     // Start is called before the first frame update
     void Start()
     {
-        
         //To-do: fazer voltar para mesma página que o player estava jogando
         currentPage = SaveSystem.GetInstance().currLevelPage;
         //desliga todos, menos a página corrente, que deve ficar ligada
+        //Pega o valor total de estrelas de cada pagina (duvido que funcione e se funcionar vai bugar a ultima pagina)
+        for(int i = 0; i < pagesContainers.Count;i++) 
+        {
+            TotalStars.Add(0);
+            for(int j = 0; j < 6;j++) 
+            {
+                TotalStars[i] += pagesContainers[i].transform.GetChild(0).gameObject.transform.GetChild(j).gameObject.GetComponent<LevelButton>().CountStars;
+            }
+        }
+
         for(int i = 0; i < pagesContainers.Count; i++)
         {
             pagesContainers[i].SetActive(i == currentPage);
@@ -39,7 +51,10 @@ public class LevelManager : MonoBehaviour
             ButtonNext.SetActive(true);
             ButtonPrevious.SetActive(true);
         }
-        
+        if(TotalStars[currentPage] < StarsToNextPage[currentPage]) 
+        {
+            ButtonNext.SetActive(false);
+        }
     }
 
     public void NextPage()
@@ -53,7 +68,7 @@ public class LevelManager : MonoBehaviour
             pagesContainers[currentPage].SetActive(true);   
         }
 
-        if(currentPage == pagesContainers.Count - 1)
+        if(currentPage == pagesContainers.Count - 1 || TotalStars[currentPage] < StarsToNextPage[currentPage])
         {
             ButtonNext.SetActive(false);
         }
