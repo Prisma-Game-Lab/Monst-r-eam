ada804d6e35f676cb95d4dd4153b160d218a4b1d
Renan da Fonte <renandafonte@hotmail.com>
2019-05-21 22:48:32 -0300

    ferramenta pros gds testarem em velocidades diferentes

---
+++ Assets/Scripts/Teste/ChangeTimeScale.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ferramenta para ajudar os gds a testarem o jogo em velocidades menores (pra garantir que coisas sejam difíceis mas não impossíveis)
+/// </summary>
+public class ChangeTimeScale : MonoBehaviour
+{
+    [Range(0,1)] public float timeScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = timeScale;
+    }
+}
